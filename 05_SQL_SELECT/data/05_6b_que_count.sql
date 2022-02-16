3fb1cbb7a3883ed7297be9c7064a64c95c96b7c7
Stephan Kruse <bobbyinsel7788@gmail.com>
2022-02-16 09:49:14 +0000

    count

--- 05_SQL_SELECT/data/05_6b_que_count.sql
+++ 05_SQL_SELECT/data/05_6b_que_count.sql
@@ -1,30 +1,58 @@
--- alle Sektoren
+/* QUERIES - count()*/
 
 
 
+/* Links:
+https://dev.mysql.com/doc/refman/5.6/en/counting-rows.html
+https://dev.mysql.com/doc/refman/5.6/en/group-by-handling.html
+https://dev.mysql.com/doc/refman/5.6/en/aggregate-functions.html#function_count-distinct
+*/
+-- Welche VERSCHIEDENEN Sektoren gibt es?
 
-/*SELECT
+/*
+SELECT
+DISTINCT sector Industriesektoren
+FROM stocks.ccc
+ORDER BY sector ASC
+;
+
+-- Welche VERSCHIEDENEN Sektoren gibt es?
+SELECT
+    count(DISTINCT sector) "Anzahl Industriesektoren"
+FROM stocks.ccc
+;
+*/
+-- Welche VERSCHIEDENEN Branchen ?
+/*
+SELECT
 DISTINCT industry Branchen
 FROM stocks.ccc
-ORDER BY industry ASC
+ORDER BY Branchen ASC
+;
+
+
+-- Welche VERSCHIEDENEN Branchen ?
+
+SELECT
+count(DISTINCT industry) "Anzahl Branchen"
+FROM stocks.ccc
+
 ;
 */
 
--- welche VERSCHIEDENEN Auszahlungs-Rhythmen gibt es?
-SELECT
-DISTINCT payouts "Auszahlungen p.a."
-FROM stocks.ccc
-ORDER BY payouts ASC;
+-- Wieviele VERSCHIEDENE Branchen gibt es
+-- in den jeweiligen Industriesektoren?
 
--- Welche Unternehmen zahlen monatlich?
-SELECT 
-  c_name Branche, 
-  industry Unternehmen,
-  ticker "DOW-Symbol"
+SELECT
+sector Industriesektoren,  -- nicht aggregiert /organisch
+count(DISTINCT industry) AS Branchen -- aggregiert, durch eine Funktion entstanden
 FROM stocks.ccc
-WHERE payouts = 12
-ORDER BY Branche ASC
+GROUP BY sector -- bei Kombination organisch, bzw. aggregiert
+ORDER BY Branchen DESC
 ;
+
+
+
 
 
 
